7cca37ebf365652224758e8a44c8e048743d571c
israel18135 <jake.pendergrass@gmail.com>
2025-04-28 13:32:19 -0500

    Completed all search and update logic

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ def search():
     global lib
     # Create search query
     search = input("Search: ").lower()
-    query = []
+    matches = 0
     for i, book in enumerate(lib.loc[:, 'Title']):
         if search in book.lower():
-            query.append((i, lib.loc[i, 'Title'], lib.loc[i, 'Obtained Status'], lib.loc[i, 'Read Status']))
-            print(f'# {i}')
-            print(f'Title: {lib.loc[i, 'Title']}')
-            print(f'Obtained Status: {lib.loc[i, 'Obtained Status']}')
-            print(f'Read Status: {lib.loc[i, 'Read Status']}\n')
+            print(f'{lib.loc[i]}\n')
+            matches += 1
+
+    # Request to try again if no matches are found
+    if matches == 0:
+        print('No matches found.  Please try again.')
+        return True
+    
+    return False
 
 def change_status():
     global lib
-    book = int(input("Choose which book: # "))
-    print(lib.loc[book])
+    book = input("Choose which book: # ")
+    
+    if book.isalnum():
+        print(lib.loc[book])
+
+    else:
+        return False
 
     status = int(input("(1) Obtained Status or (2) Read Status: "))
     if status == 1:
@@ -34,12 +43,19 @@ def change_status():
             lib.loc[book, 'Read Status'] = 'Not Read'
 
     print(lib.loc[book])
+    lib.to_csv('tables.csv', index=False)
+    print("Update complete\n")
+    return input("Change another status? (boolean variable value): ")
 
 
 # Pull library
 lib = pd.read_csv('tables.csv')
 
-
-
-# TODO: Write changes to csv
-# TODO: Make search function and update status function
+searching = True
+updating = True
+while searching:
+    searching = search() # Returns false if searching is done
+    if not searching:
+        while updating:
+            updating = change_status()
+        searching = input("Search again? (boolean variable value): ")
